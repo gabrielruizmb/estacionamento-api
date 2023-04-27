edaba316f83b175536d9bde8523402464034c198
Gabriel Ruiz Mussi Bersot <gabriel@debian-BULLSEYE-live-builder-AMD64>
2023-04-26 21:42:40 -0300

    Adcionado anotações @Repository nos repositorys

---
+++ estacionamento/src/main/java/br/com/uniamerica/estacionamento/controller/ModeloController.java
@@ -0,0 +1,9 @@
+package br.com.uniamerica.estacionamento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api/modelo")
+public class ModeloController {
+}
